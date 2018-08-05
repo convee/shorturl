7d1c133c2b4c4b863f690ee5df261fd379b55b09
convee <convee.cn@gmail.com>
2018-08-05 22:28:54 +0800

    62进制算法

---
+++ app/url.go
@@ -0,0 +1,26 @@
+package app
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	CHARS  = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+	LENGTH = 6
+)
+
+func GenShortUrl(longUrl string) {
+}
+
+func GeneralShortgUrl(longUrl string) string {
+	var shortUrl string
+	urlCharsLen := len(CHARS)
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < LENGTH; i++ {
+		randNumber := r.Intn(urlCharsLen)
+		shortUrl += CHARS[randNumber : randNumber+1]
+	}
+	return shortUrl
+}
--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"net/http"
+
 	"github.com/convee/goboot"
 	"github.com/convee/shorturl/app"
+	"github.com/convee/shorturl/util"
+)
+
+var (
+	addr = *flag.String("addr", "127.0.0.1:8001", "http server")
 )
 
 func main() {
+	flag.Parse()
 	goboot.Run("config.toml")
-	app.NewModel().GetAllShorturl()
-	app.NewModel().GetShorturl()
+	DecimalTo62()
+	startHTTPServer(addr)
+
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+func startHTTPServer(addr string) {
+	fmt.Println("http server starting....")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(addr, nil)
+}
+func GenShort() {
+	longUrl := "https://www.baidu.com"
+	shortUrl := app.GeneralShortgUrl(longUrl)
+	fmt.Println(shortUrl)
+}
+
+func DecimalTo62() {
+	shortUrl := util.DecimalToAny(10000000000, 62)
+	fmt.Println(shortUrl)
 }
